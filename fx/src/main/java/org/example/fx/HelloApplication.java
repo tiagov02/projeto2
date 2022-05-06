11ddaa2cded4608417cee467881fb3989eedfa33
Luís Filipe Malheiro <ismalheiro1@gmail.com>
2022-05-06 22:56:02 +0100

    pagina inicial do utilizador mas com erros porque não podemos usar fontawsome a não ser que instalemos a biblioteca atraves de código...O mais fácil é usar iamgens como icons

--- fx/src/main/java/org/example/fx/HelloApplication.java
+++ fx/src/main/java/org/example/fx/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 755, 539);
+        Scene scene = new Scene(fxmlLoader.load(), 755, 537);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
