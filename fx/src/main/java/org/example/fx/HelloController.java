046b1890504400682175bde7104e661c270ac706
Tiago Viana <tiagoviana.1003@gmail.com>
2022-05-06 20:06:39 +0100

    0605

--- fx/src/main/java/org/example/fx/HelloController.java
+++ fx/src/main/java/org/example/fx/HelloController.java
@@ -2,11 +2,13 @@ package org.example.fx;
 
 import com.example.bd.CRUD.ClienteCRUD;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Button BtnInicio;
 
     @FXML
     protected void onHelloButtonClick() {
