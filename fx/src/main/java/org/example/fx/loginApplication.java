862db00c574bdcaf04ed96d00fec3bc2208c69c9
Tiago Viana <tiagoviana.1003@gmail.com>
2022-05-12 12:02:32 +0100

    Singleton da Classe Colab--> user logado

--- fx/src/main/java/org/example/fx/loginApplication.java
+++ fx/src/main/java/org/example/fx/loginApplication.java
@@ -4,11 +4,15 @@ import com.example.bd.CRUD.ColaboradorCRUD;
 import com.example.bd.Entity.Cliente;
 import com.example.bd.Entity.Colaborador;
 import org.example.fx.Exceptions.UserPwdErradoaxception;
+import org.example.fx.SingleInstance.UserLogado;
+
+import java.util.Arrays;
 
 public class loginApplication {
    public Colaborador verifyLogin(String username,byte[] pwd) throws UserPwdErradoaxception {
       for(Colaborador c: ColaboradorCRUD.findTodosColaboradores()){
-         if(c.getUsername().equals(username) && c.getPassword().equals(pwd.toString())){
+         if(c.getUsername().equals(username) && c.getPassword().equals(Arrays.toString(pwd))){
+            UserLogado.getInstance().setCurrentUser(c);
             return c;
          }
       }
