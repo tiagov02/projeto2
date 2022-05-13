54429f1091097a6cfc369919937fdbceb1bda9bb
Tiago Viana <tiagoviana.1003@gmail.com>
2022-05-13 20:19:56 +0100

    13/05

--- fx/src/main/java/org/example/fx/HelloApplication.java
+++ fx/src/main/java/org/example/fx/HelloApplication.java
@@ -7,10 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.PopupControl;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -49,8 +46,12 @@ public class HelloApplication extends Application {
             TrocaPaineis.changePanel(event,"menuPrincipalGerente.fxml","Menu do Gerente",ColaboradorController.class);
         }
         catch (Exception ex){
-            ex.printStackTrace();
+            Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+            dialogoAviso.setTitle("ERRO!!");
+            dialogoAviso.setHeaderText(ex.getMessage());
+            dialogoAviso.showAndWait();
         }
+        //
     }
 
     public static void main(String[] args) throws Exception {
