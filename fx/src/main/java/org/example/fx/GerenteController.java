5d16db35d1cff445e7b9cd61e52e3303a3e8f866
lmalheiro7 <lmalheiro@ipvc.pt>
2022-05-18 23:21:29 +0100

    teste a minha nova BD

--- fx/src/main/java/org/example/fx/GerenteController.java
+++ fx/src/main/java/org/example/fx/GerenteController.java
@@ -64,6 +64,28 @@ public class GerenteController{
     private TableView<Encomendafornecedor> table;
 
 
+    public void clicaPaginaPrincipal(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerenteMenuPrincipal.fxml", "Pagina Principal", GerenteController.class);
+    }
+
+    public void clicaListaCompras(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerenteListaCompras.fxml", "Lista Compras", GerenteController.class);
+    }
+
+    public void clicaAtualizaStockProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerenteAtualizaStocks.fxml", "Atualiza Stocks", GerenteController.class);
+    }
+
+    public void clicaDefinicoesColaborador(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesColaborador.fxml", "Definicoes Colaborador", GerenteController.class);
+    }
+    public void clicaDefinicoesProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesProdutos.fxml", "Definicoes Produtos", GerenteController.class);
+    }
+
+    public void clicaDefinicoesClientes(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesClientes.fxml", "Definicoes Clientes", GerenteController.class);
+    }
 
     public void ClicaListarEncomendas(javafx.event.ActionEvent event) throws IOException {
         TrocaPaineis.changePanel(event,"GerenteListarEncomendas.fxml","Listagem de encomendas",GerenteController.class);
