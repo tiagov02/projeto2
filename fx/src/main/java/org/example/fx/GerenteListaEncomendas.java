5234a7373701356c9247d6b70cefee3695af5389
lmalheiro7 <lmalheiro@ipvc.pt>
2022-06-03 15:11:11 +0100

    upload

--- fx/src/main/java/org/example/fx/GerenteListaEncomendas.java
+++ fx/src/main/java/org/example/fx/GerenteListaEncomendas.java
@@ -1,4 +1,15 @@
 package org.example.fx;
 
-public class GerenteListaEncomendas {
+import com.example.bd.Entity.Linhaencomendafornecedor;
+import com.example.bd.Entity.Produto;
+import com.example.bd.Entity.Tipoproduto;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GerenteListaEncomendas{
+
 }
