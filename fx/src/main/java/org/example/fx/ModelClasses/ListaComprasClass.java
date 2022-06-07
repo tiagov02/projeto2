874987799d7a30aaf2031834e184a204fe00c0ee
Tiago Viana <tiagoviana.1003@gmail.com>
2022-06-07 12:01:29 +0100

    adicionei classes modelo para listar nas tables

--- fx/src/main/java/org/example/fx/ModelClasses/ListaComprasClass.java
+++ fx/src/main/java/org/example/fx/ModelClasses/ListaComprasClass.java
@@ -1,4 +1,10 @@
 package org.example.fx.ModelClasses;
 
+import javafx.collections.ObservableList;
+
 public class ListaComprasClass {
+    private String tipoProduto;
+    private String produto;
+    private int qtdExistente;
+    private int qtdComprar;
 }
