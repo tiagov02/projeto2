bdcc050b07e59ad7080f645e44a88db6524aaa4a
lmalheiro7 <lmalheiro@ipvc.pt>
2022-06-08 22:12:18 +0100

    update erro resolvido

--- fx/src/main/java/org/example/fx/AdicionarProdutos.java
+++ fx/src/main/java/org/example/fx/AdicionarProdutos.java
@@ -1,4 +1,67 @@
 package org.example.fx;
 
+import com.example.bd.Entity.Tipoproduto;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import org.example.fx.Logica.TrocaPaineis;
+
+import java.io.IOException;
+
 public class AdicionarProdutos {
+
+    @FXML
+    private TextField labelnumproduto;
+    @FXML
+    private TextField labelnomeproduto;
+    @FXML
+    private TextField labelqtdstock;
+    @FXML
+    private TextField labelqtdcomprar;
+    @FXML
+    private ComboBox<Tipoproduto> comboBox;
+
+
+
+    public void backbutton(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event, "GerentreListaCompras.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void cleanButton(){
+        labelnumproduto.setText("");
+        labelnomeproduto.setText("");
+        labelqtdcomprar.setText("");
+        labelqtdstock.setText("");
+    }
+
+    public void clicaadicionarProduto(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event, "AdicionarProduto.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaPaginaPrincipal(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event, "GerenteMenuPrincipal.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaListaCompras(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentreListaCompras.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaAtualizaStockProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerenteAtualizaStocks.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaDefinicoesColaborador(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesColaborador.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+    public void clicaDefinicoesProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesProdutos.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaDefinicoesClientes(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "GerentedefinicoesClientes.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void ClicaListarEncomendas(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event,"GerenteListarEncomendas.fxml","Listagem de encomendas",GerenteController.class);
+    }
 }
