e655dc0e1d8ee45e8a3337601b8306f9e2a06f65
lmalheiro7 <lmalheiro@ipvc.pt>
2022-06-18 22:57:40 +0100

    update colaborador finish

--- fx/src/main/java/org/example/fx/ColaboradorAtualizaStocksController.java
+++ fx/src/main/java/org/example/fx/ColaboradorAtualizaStocksController.java
@@ -1,11 +1,18 @@
 package org.example.fx;
 
 import com.example.bd.CRUD.ProdutoCRUD;
+import com.example.bd.CRUD.exceptions.IdNaoEncontradoException;
 import com.example.bd.Entity.Produto;
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.DefaultStringConverter;
+import javafx.util.converter.FloatStringConverter;
+import javafx.util.converter.IntegerStringConverter;
 import org.example.fx.Logica.TrocaPaineis;
 
 import java.io.IOException;
@@ -15,84 +22,191 @@ import java.util.ResourceBundle;
 
 public class ColaboradorAtualizaStocksController implements Initializable {
 
-    @FXML
-    private TextField lbl_nomeProd;
 
     @FXML
-    private TextField lbl_qtdMinima;
-
+    private TableColumn<ProdutoTipo, Integer> numproduto;
     @FXML
-    private TextField lbl_qtdStock;
-
+    private TableColumn<ProdutoTipo, String> nomeproduto;
     @FXML
-    private TextField lbl_valUnitario;
-
+    private TableColumn<ProdutoTipo, Integer> tipoproduto;
     @FXML
-    private ComboBox<?> sel_tipoProd;
-
+    private TableColumn<ProdutoTipo, Integer> qtdstock;
     @FXML
-    private Button OKButton;
+    private TableColumn<ProdutoTipo, Integer> qtdminima;
     @FXML
-    private TableColumn<Produto, Integer> tbl_QtdStock;
-
+    private TableColumn<ProdutoTipo, Float> valor;
     @FXML
-    private TableColumn<Produto, Integer> tbl_qtdMinima;
-
-    @FXML
-    private TableColumn<Produto, BigDecimal> tbl_valUnitario;
-
-    @FXML
-    private TableView<Produto> table_produtos;
-
-    @FXML
-    private TableColumn<Produto, String> tbl_nomeProd;
-
-    @FXML
-    private TableColumn<Produto, Integer> tbl_tipoProduto;
-
+    public TableView<ProdutoTipo> tableproduto;
     @FXML
     private Button btn_pesq;
-
     @FXML
     private TextField lbl_codBarras;
 
-    public void clicaPaginaPrincipal(javafx.event.ActionEvent event) throws IOException {
-        TrocaPaineis.changePanel(event, "ColaboradorMenuPrincipal.fxml", "Loja Produtos Biológicos", ColaboradorController.class);
-        //Cliente[] clientes = listarCliMaisGasto();
-        //VAI IMPLEMENTAR INICIALIZABLE
+
+    public void updateNomeProduto(){
+        nomeproduto.setCellFactory(TextFieldTableCell.forTableColumn(new DefaultStringConverter()));
+        nomeproduto.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ProdutoTipo, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ProdutoTipo, String> produtoTipoStringCellEditEvent) {
+                ProdutoTipo prod = produtoTipoStringCellEditEvent.getRowValue();
+                prod.setNome(produtoTipoStringCellEditEvent.getNewValue());
+                Produto p=ProdutoCRUD.findProduto(prod.getId());
+                p.setNome(prod.getNome());
+                try {
+                    ProdutoCRUD.editProduto(p);
+                } catch (IdNaoEncontradoException ex){
+                    Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+                    dialogoAviso.setTitle("ERRO!!");
+                    dialogoAviso.setHeaderText(ex.getMessage());
+                    dialogoAviso.showAndWait();
+                }
+            }
+        });
     }
 
-
-    public void clicaListaCompras(javafx.event.ActionEvent event) throws IOException{
-        TrocaPaineis.changePanel(event, "ColaboradorListaCompras.fxml", "Loja Produtos Biológicos", ColaboradorListaComprasController.class);
+    public void updateQtdStock(){
+        qtdstock.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        qtdstock.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ProdutoTipo, Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ProdutoTipo, Integer> produtoTipoIntegerCellEditEvent) {
+                ProdutoTipo prod = produtoTipoIntegerCellEditEvent.getRowValue();
+                prod.setQtdStock(produtoTipoIntegerCellEditEvent.getNewValue());
+                Produto p=ProdutoCRUD.findProduto(prod.getId());
+                if (prod.getId() == p.getNumproduto()){
+                    p.setQuantidadestock(prod.getQtdStock());
+                    try {
+                        ProdutoCRUD.editProduto(p);
+                    } catch (IdNaoEncontradoException ex){
+                        Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+                        dialogoAviso.setTitle("ERRO!!");
+                        dialogoAviso.setHeaderText(ex.getMessage());
+                        dialogoAviso.showAndWait();
+                    }
+                }
+            }
+        });
     }
 
-    public void clicaListaEncomendas(javafx.event.ActionEvent event) throws IOException{
-        TrocaPaineis.changePanel(event, "ColaboradorListaEncomendas.fxml", "Loja Produtos Biológicos", ColaboradorListaEncomendasController.class);
+    public void updateQtdMinima(){
+        qtdminima.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        qtdminima.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ProdutoTipo, Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ProdutoTipo, Integer> produtoTipoIntegerCellEditEvent) {
+                ProdutoTipo prod = produtoTipoIntegerCellEditEvent.getRowValue();
+                prod.setQtdMinima(produtoTipoIntegerCellEditEvent.getNewValue());
+                Produto p=ProdutoCRUD.findProduto(prod.getId());
+                p.setQuantidademinima(prod.getQtdMinima());
+                try {
+                    ProdutoCRUD.editProduto(p);
+                } catch (IdNaoEncontradoException ex){
+                    Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+                    dialogoAviso.setTitle("ERRO!!");
+                    dialogoAviso.setHeaderText(ex.getMessage());
+                    dialogoAviso.showAndWait();
+                }
+            }
+        });
     }
 
-    public void clicaAtualizaStocks(javafx.event.ActionEvent event) throws IOException{
-        TrocaPaineis.changePanel(event, "ColaboradorAtualizaStocks.fxml", "Loja Produtos Biológicos", ColaboradorAtualizaStocksController.class);
+    public void updateValorUnitario(){
+        valor.setCellFactory(TextFieldTableCell.forTableColumn(new FloatStringConverter()));
+        valor.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ProdutoTipo, Float>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ProdutoTipo, Float> produtoTipoFloatCellEditEvent) {
+                ProdutoTipo prod = produtoTipoFloatCellEditEvent.getRowValue();
+                prod.setValUnit(produtoTipoFloatCellEditEvent.getNewValue());
+                Produto p = ProdutoCRUD.findProduto(prod.getId());
+                p.setValorunitariototal(new BigDecimal(prod.getValUnit()));
+                try {
+                    ProdutoCRUD.editProduto(p);
+                } catch (IdNaoEncontradoException ex){
+                    Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+                    dialogoAviso.setTitle("ERRO!!");
+                    dialogoAviso.setHeaderText(ex.getMessage());
+                    dialogoAviso.showAndWait();
+                }
+            }
+        });
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        tbl_nomeProd.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        tbl_tipoProduto.setCellValueFactory(new PropertyValueFactory<>("idtipoproduto"));
-        tbl_QtdStock.setCellValueFactory(new PropertyValueFactory<>("quantidadestock"));
-        tbl_qtdMinima.setCellValueFactory(new PropertyValueFactory<>("quantidademinima"));
-        tbl_valUnitario.setCellValueFactory(new PropertyValueFactory<>("valorunitariototal"));
-
-        for(Produto p: ProdutoCRUD.findTodosProdutos()){
-            table_produtos.getItems().add(p);
+        tableproduto.setEditable(true);
+        updateNomeProduto();
+        updateQtdStock();
+        updateQtdMinima();
+        updateValorUnitario();
+        numproduto.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nomeproduto.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tipoproduto.setCellValueFactory(new PropertyValueFactory<>("tipoProduto"));
+        qtdstock.setCellValueFactory(new PropertyValueFactory<>("qtdStock"));
+        qtdminima.setCellValueFactory(new PropertyValueFactory<>("qtdMinima"));
+        valor.setCellValueFactory(new PropertyValueFactory<>("valUnit"));
+        for(Produto p:ProdutoCRUD.findTodosProdutos()){
+            ProdutoTipo pt= new ProdutoTipo();
+            pt.setId(p.getNumproduto());
+            pt.setNome(p.getNome());
+            pt.setQtdStock(p.getQuantidadestock());
+            pt.setQtdMinima(p.getQuantidademinima());
+            pt.setValUnit(p.getValorunitariototal().floatValue());
+            pt.setTipoProduto(p.getTipoprodutoByIdtipoproduto().getSeccao());
+            tableproduto.getItems().add(pt);
         }
     }
 
-    public void clickOK(javafx.event.ActionEvent event){
-        Produto pro=table_produtos.getSelectionModel().getSelectedItem();
-        lbl_nomeProd.setText(pro.getNome());
-        lbl_qtdMinima.setText(pro.getQuantidademinima().toString());
-        lbl_qtdStock.setText(pro.getQuantidadestock().toString());
-        lbl_valUnitario.setText(pro.getValorunitario().toString());
+    public void pesquisa(javafx.event.ActionEvent event){
+        int codBarras=0;
+
+        if (lbl_codBarras.getText().equals("")){
+            initialize(null, null);
+            return;
+        }
+        try{
+            codBarras=Integer.parseInt(lbl_codBarras.getText());
+        }
+        catch(NumberFormatException ex){
+            Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+            dialogoAviso.setTitle("ERRO!!");
+            dialogoAviso.setHeaderText("Erro! Não pode introduzir letras no cod de barras!!");
+            dialogoAviso.showAndWait();
+        }
+        Produto p=ProdutoCRUD.findProduto(codBarras);
+        if(p==null){
+            Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+            dialogoAviso.setTitle("ERRO!!");
+            dialogoAviso.setHeaderText("Erro! Não existe nenhum produto com esse cod de barras");
+            dialogoAviso.showAndWait();
+        }
+        else{
+            tableproduto.setItems(FXCollections.observableArrayList());
+            ProdutoTipo pt= new ProdutoTipo();
+            pt.setId(p.getNumproduto());
+            pt.setNome(p.getNome());
+            pt.setQtdStock(p.getQuantidadestock());
+            pt.setQtdMinima(p.getQuantidademinima());
+            pt.setValUnit(p.getValorunitariototal().floatValue());
+            pt.setTipoProduto(p.getTipoprodutoByIdtipoproduto().getSeccao());
+            tableproduto.getItems().add(pt);
+        }
     }
+
+    public void clicaPaginaPrincipal(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event, "ColaboradorMenuPrincipal.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaListaCompras(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ColaboradorListaCompras.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void ClicaListarEncomendas(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event,"ColaboradorListaEncomendas.fxml","Listagem de encomendas",GerenteController.class);
+    }
+
+    public void clicaAtualizaStockProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ColaboradorAtualizaStocks.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+    public void clicaLogout(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ConfirmacaoSaida.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
 }
--- fx/src/main/java/org/example/fx/ColaboradorListaEncomendasController.java
+++ fx/src/main/java/org/example/fx/ColaboradorListaEncomendasController.java
@@ -1,4 +1,122 @@
 package org.example.fx;
 
-public class ColaboradorListaEncomendasController {
+import com.example.bd.CRUD.EstadoFaturaCRUD;
+import com.example.bd.CRUD.FaturaCRUD;
+import com.example.bd.Entity.Estadofatura;
+import com.example.bd.Entity.Fatura;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.example.fx.Logica.TrocaPaineis;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ColaboradorListaEncomendasController implements Initializable {
+
+    @FXML
+    private TableColumn<ListaEncomendas, Integer> numfatura;
+
+    @FXML
+    private TableColumn<ListaEncomendas, String> nomecliente;
+
+    @FXML
+    private TableColumn<ListaEncomendas, String> moradacliente;
+
+    @FXML
+    private TableColumn<ListaEncomendas, String> telefonecliente;
+
+    @FXML
+    private TableColumn<ListaEncomendas, Float> valortotalfatura;
+
+    @FXML
+    private TableColumn<ListaEncomendas, String> estadofatura;
+
+    @FXML
+    private TableView<ListaEncomendas> tablelistaencomenda;
+
+    @FXML
+    private Button btn_procura;
+
+    @FXML
+    private TextField lbl_cliente;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int idEstado=0;
+        numfatura.setCellValueFactory(new PropertyValueFactory<>("numFatura"));
+        nomecliente.setCellValueFactory(new PropertyValueFactory<>("nomeCliente"));
+        moradacliente.setCellValueFactory(new PropertyValueFactory<>("morada"));
+        telefonecliente.setCellValueFactory(new PropertyValueFactory<>("telefoneCliente"));
+        valortotalfatura.setCellValueFactory(new PropertyValueFactory<>("valorTotal"));
+        estadofatura.setCellValueFactory(new PropertyValueFactory<>("estadoFatura"));
+        for (Fatura fat: FaturaCRUD.findTodasFaturas()){
+            ListaEncomendas lista = new ListaEncomendas();
+            lista.setNumFatura(fat.getNumfatura());
+            lista.setNomeCliente(fat.getClienteByIdcliente().getNome());
+            lista.setMorada(fat.getMoradaentregaByIdentrega().getCodpostal() + "  " + fat.getMoradaentregaByIdentrega().getRua() + "  " + fat.getMoradaentregaByIdentrega().getNumporta());
+            lista.setTelefoneCliente(fat.getClienteByIdcliente().getTelefone());
+            lista.setValorTotal(fat.getValorfatura().floatValue());
+            tablelistaencomenda.getItems().add(lista);
+        }
+    }
+
+    public void pesquisa(javafx.event.ActionEvent event){
+        tablelistaencomenda.setItems(FXCollections.observableArrayList());
+        int cont=0;
+        if (lbl_cliente.getText().equals("")){
+            initialize(null, null);
+            return;
+        }
+        for (Fatura fat: FaturaCRUD.findTodasFaturas()){
+            if(lbl_cliente.getText().equals(Integer.toString(fat.getIdcliente())) ||
+                    lbl_cliente.getText().equals(fat.getClienteByIdcliente().getNome()) ||
+                    lbl_cliente.getText().equals(fat.getClienteByIdcliente().getTelefone())){
+                cont++;
+                ListaEncomendas lista = new ListaEncomendas();
+                lista.setNumFatura(fat.getNumfatura());
+                lista.setNomeCliente(fat.getClienteByIdcliente().getNome());
+                lista.setMorada(fat.getMoradaentregaByIdentrega().getCodpostal() + "  " + fat.getMoradaentregaByIdentrega().getRua() + "  " + fat.getMoradaentregaByIdentrega().getNumporta());
+                lista.setTelefoneCliente(fat.getClienteByIdcliente().getTelefone());
+                lista.setValorTotal(fat.getValorfatura().floatValue());
+                for(Estadofatura ef: EstadoFaturaCRUD.findAllEstadosFaturas()){
+                    if(ef.getNumfatura()== fat.getNumfatura()){
+                        lista.setEstadoFatura(ef.getEstadoByIdestado().getDescricao());
+                    }
+                }
+                tablelistaencomenda.getItems().add(lista);
+            }
+        }
+        if(cont==0){
+            Alert dialogoAviso = new Alert(Alert.AlertType.WARNING);
+            dialogoAviso.setTitle("ERRO!!");
+            dialogoAviso.setHeaderText("Erro! Não existe nenhuma encomenda para o cliente referido!!");
+            dialogoAviso.showAndWait();
+        }
+    }
+
+    public void clicaPaginaPrincipal(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event, "ColaboradorMenuPrincipal.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void clicaListaCompras(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ColaboradorListaCompras.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+
+    public void ClicaListarEncomendas(javafx.event.ActionEvent event) throws IOException {
+        TrocaPaineis.changePanel(event,"ColaboradorListaEncomendas.fxml","Listagem de encomendas",GerenteController.class);
+    }
+
+    public void clicaAtualizaStockProdutos(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ColaboradorAtualizaStocks.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
+    public void clicaLogout(javafx.event.ActionEvent event) throws IOException{
+        TrocaPaineis.changePanel(event, "ConfirmacaoSaida.fxml", "Loja Produtos Biológicos", GerenteController.class);
+    }
 }
