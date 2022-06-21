e8f5710ee7d403fbde2a5a6711df9db0ad43c13f
Tiago Viana <tiagoviana.1003@gmail.com>
2022-06-21 14:40:30 +0100

    .

--- springwebmvc/src/main/java/com/example/springwebmvc/LoginController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.springwebmvc.controller;
+package com.example.springwebmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- springwebmvc/src/main/java/com/example/springwebmvc/NavigationController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/NavigationController.java
@@ -1,4 +1,4 @@
-package com.example.springwebmvc.controller;
+package com.example.springwebmvc;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
