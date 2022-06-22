b6661e016062479eeade9744923e941d5d4e91e7
Luís Filipe Malheiro <ismalheiro1@gmail.com>
2022-06-22 17:51:09 +0100

    não funciona springweb

--- springwebmvc/src/main/java/com/example/springwebmvc/NavigationController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/NavigationController.java
@@ -1,14 +1,19 @@
 package com.example.springwebmvc;
 
+import com.example.bd.CRUD.ColaboradorCRUD;
+import com.example.bd.Entity.Cliente;
+import com.example.bd.Entity.Colaborador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class NavigationController {
-
-    @GetMapping("/")
-    public String getLogin(){
-        return "login";
+    @GetMapping("")
+    public String showHomePage(){
+        return "index";
     }
+
 }
