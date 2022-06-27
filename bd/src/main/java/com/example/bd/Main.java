6ba16798446b75c851bf4a8d8290c75114b20805
Luís Filipe Malheiro <ismalheiro1@gmail.com>
2022-06-27 23:39:13 +0100

    UPDATE index...Template sacado da net... Eu faço a parte toda HTML de frontend Depois fazemos juntos as funções

--- bd/src/main/java/com/example/bd/Main.java
+++ bd/src/main/java/com/example/bd/Main.java
@@ -1,9 +1,18 @@
 package com.example.bd;
 
 import com.example.bd.CRUD.ClienteCRUD;
+import com.example.bd.Encrypt.Encriptacao;
 import com.example.bd.Entity.Cliente;
 
 public class Main {
     public static void main(String[] args) {
+
+        Cliente c1 = new Cliente();
+        try {
+            c1.setPassword(Encriptacao.encript("teste"));
+        } catch (Exception ex){
+            System.out.println("teste");
+        }
+
     }
 }
