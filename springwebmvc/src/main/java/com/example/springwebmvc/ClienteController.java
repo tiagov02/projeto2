b5828429ac104c02c0fae856ce0d6c4f6b672c85
Tiago Viana <tiagoviana.1003@gmail.com>
2022-06-29 03:30:34 +0100

    update

--- springwebmvc/src/main/java/com/example/springwebmvc/ClienteController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/ClienteController.java
@@ -1,14 +1,12 @@
 package com.example.springwebmvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NavigationController {
-
-    @GetMapping("/")
-    public String getIndex(){
-        return "index";
+public class ClienteController {
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
     }
 }
