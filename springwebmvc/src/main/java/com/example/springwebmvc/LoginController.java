972e608b2cef308c75835451826c83258c076f05
Tiago Viana <tiagoviana.1003@gmail.com>
2022-07-02 00:23:31 +0100

    registar a funcionar

--- springwebmvc/src/main/java/com/example/springwebmvc/LoginController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/LoginController.java
@@ -21,18 +21,11 @@ public class LoginController {
 
     @PostMapping(value = "/plogin")
     public String verificaLogin(@ModelAttribute LoginData login, Model model){
-        System.out.println("USER" + login.getNomeUser() + "PASSWORD" + login.getPassword());
-        try{
             try {
                 Cliente cli = ClienteCRUD.login(login.getNomeUser(), Encriptacao.encript(login.getPassword()));
                 return "clientelogado";
             } catch (Exception ex){
                 return "error";
             }
-
-        }catch (NoResultException ex){
-            //model.addAttribute("erro", "Username ou password erradas");
-            return "error";
-        }
     }
 }
