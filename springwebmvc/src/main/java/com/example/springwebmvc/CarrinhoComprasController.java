8ecbb223e0692f77d521921c878617a74138b6d5
Tiago Viana <tiagoviana.1003@gmail.com>
2022-07-03 01:05:57 +0100

    erro ao passar

--- springwebmvc/src/main/java/com/example/springwebmvc/CarrinhoComprasController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/CarrinhoComprasController.java
@@ -7,6 +7,7 @@ import com.example.springwebmvc.ModelClasses.ModelLinhaFatura;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 public class CarrinhoComprasController {
+
     @GetMapping(value="/addCarrinhoCompras/{idprod}")
     public String addCarrinhoCompras(HttpSession session,@PathVariable int idprod){
         int qtd=1;
