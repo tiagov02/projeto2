439824f492368105f46ef8b29ad24e972bd35279
Luís Malheiro <lmalheiro@ipvc.pt>
2022-07-06 19:29:28 +0100

    update

--- fx/src/main/java/org/example/fx/MainTeste.java
+++ fx/src/main/java/org/example/fx/MainTeste.java
@@ -5,6 +5,7 @@ import com.example.bd.Encrypt.Encriptacao;
 import com.example.bd.Entity.*;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class MainTeste {
 
@@ -12,5 +13,13 @@ public class MainTeste {
     public static void main(String[] args) throws Exception {
         //Codpostais cod=CodPostaisCRUD.findCodPostal("0989-777");
         //System.out.println("Cod postal: "+cod.getCodpostal()+"\nLocalidade : "+cod.getLocalidade());
+
+
+        Fatura f = new Fatura();
+        f.setIdcliente(1);
+        f.setIdcolaborador(1);
+        f.setIdentrega(1);
+        f.setValorfatura(new BigDecimal(2000.12));
+        FaturaCRUD.createFatura(f);
     }
 }
