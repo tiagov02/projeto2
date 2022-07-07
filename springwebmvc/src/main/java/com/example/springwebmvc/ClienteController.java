b925ffdd81b821d3b8fe4d9e7bf84964b931ee21
Tiago Viana <tiagoviana.1003@gmail.com>
2022-07-07 20:16:58 +0100

    update não terminado formulário de alteração de user

--- springwebmvc/src/main/java/com/example/springwebmvc/ClienteController.java
+++ springwebmvc/src/main/java/com/example/springwebmvc/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.springwebmvc;
 import com.example.bd.CRUD.ClienteCRUD;
 import com.example.bd.CRUD.ProdutoCRUD;
 import com.example.bd.Entity.Cliente;
+import com.example.springwebmvc.ModelClasses.ModelCliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,60 @@ public class ClienteController {
     public String getIndex(){
         return "redirect:/";
     }
+
+    @GetMapping("/meusdetalhes")
+    public String getDetalhesCli(HttpSession session, Model model){
+        if(session.getAttribute("UserLogged")==null){
+            return "redirect:/login";
+        }
+        Cliente cliTemp=((Cliente) session.getAttribute("UserLogged"));
+        ModelCliente cli=new ModelCliente();
+        cli.setIdtipocliente(1);
+        cli.setCodpostal(cliTemp.getCodpostal());
+        cli.setLocalidade(cliTemp.getCodpostaisByCodpostal().getLocalidade());
+        cli.setCodpostal(cliTemp.getCodpostal());
+        cli.setTelefone(cliTemp.getTelefone());
+        cli.setNome(cliTemp.getNome());
+        //Retirar
+        cli.setPassword(cliTemp.getPassword());
+        cli.setIdcliente(cliTemp.getIdcliente());
+        cli.setNumporta(cliTemp.getNumporta());
+        cli.setRua(cliTemp.getRua());
+        cli.setUsername(cliTemp.getUsername());
+        String morCli=cli.getRua()+" , "+cli.getNumporta()+" , "+cli.getCodpostal()+" , "+cli.getLocalidade();
+
+        model.addAttribute("cli",cli);
+        model.addAttribute("nomeuser",cli.getNome());
+        model.addAttribute("morcli",morCli);
+        return "meusdetalhes";
+    }
+
+    @GetMapping("/alterauser")
+    public String getFormAlteraUser(HttpSession session,Model model){
+        if(session.getAttribute("UserLogged") == null){
+            return "redirect:/login";
+        }
+        Cliente cliTemp=((Cliente) session.getAttribute("UserLogged"));
+        ModelCliente cli=new ModelCliente();
+        cli.setIdtipocliente(1);
+        cli.setCodpostal(cliTemp.getCodpostal());
+        cli.setLocalidade(cliTemp.getCodpostaisByCodpostal().getLocalidade());
+        cli.setCodpostal(cliTemp.getCodpostal());
+        cli.setTelefone(cliTemp.getTelefone());
+        cli.setNome(cliTemp.getNome());
+        //Retirar
+        cli.setPassword(cliTemp.getPassword());
+        cli.setIdcliente(cliTemp.getIdcliente());
+        cli.setNumporta(cliTemp.getNumporta());
+        cli.setRua(cliTemp.getRua());
+        cli.setUsername(cliTemp.getUsername());
+
+        model.addAttribute("cli",cli);
+        return "mudarcliente";
+    }
+    /**
+     * CRIAR POSTMAPPING
+     */
 
 
 }
