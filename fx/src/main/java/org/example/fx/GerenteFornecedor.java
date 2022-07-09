a0115b06834d4ae8b07f63e27db7fca12b0a72cd
Luís Malheiro <lmalheiro@ipvc.pt>
2022-07-09 16:53:41 +0100

    update

--- fx/src/main/java/org/example/fx/GerenteFornecedor.java
+++ fx/src/main/java/org/example/fx/GerenteFornecedor.java
@@ -1,19 +1,59 @@
 package org.example.fx;
 
+import com.example.bd.CRUD.ColaboradorCRUD;
+import com.example.bd.CRUD.FornecedorCRUD;
+import com.example.bd.Entity.Colaborador;
+import com.example.bd.Entity.Fornecedor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.example.fx.Logica.TrocaPaineis;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class GerenteFornecedor implements Initializable {
+    @FXML
+    private TableColumn<Fornecedor, Integer> colNumero;
+    @FXML
+    private TableColumn<Fornecedor, String> colFornecedor;
+    @FXML
+    private TableColumn<Fornecedor, Integer> colMorada;
+    @FXML
+    private TableColumn<Fornecedor, String> colEmail;
+    @FXML
+    private TableColumn<Fornecedor, String> colTelefone;
+    @FXML
+    private TableView<Fornecedor> tableFornecedores;
 
 
+
+    public ObservableList<Fornecedor> getFornecedores(){
+        ObservableList<Fornecedor> fornecedors = FXCollections.observableArrayList();
+        List<Fornecedor> fornecedorList = FornecedorCRUD.findFornecedores();
+        fornecedors.addAll(fornecedorList);
+
+        return fornecedors;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        //tableFornecedores.setEditable(true);
+        for (Fornecedor f : FornecedorCRUD.findFornecedores()){
+            colNumero.setCellValueFactory(new PropertyValueFactory<>("idfornecedor"));
+            colFornecedor.setCellValueFactory(new PropertyValueFactory<>("nome"));
+            colMorada.setCellValueFactory(new PropertyValueFactory<>("rua"));
+            colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+            colTelefone.setCellValueFactory(new PropertyValueFactory<>("numtelefone"));
+            tableFornecedores.setItems(getFornecedores());
+        }
     }
 
 
